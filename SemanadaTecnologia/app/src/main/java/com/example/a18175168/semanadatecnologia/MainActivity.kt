361a0cef88601b0fd0a3a7d40497a1b46028c611
KAILANY SOUSA DA GAMA <18175168@CFP127ALUNO.LOCAL>
2019-10-11 08:24:56 -0300

    Versão 00 - Start do projeto

---
+++ SemanadaTecnologia/app/src/main/java/com/example/a18175168/semanadatecnologia/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.a18175168.semanadatecnologia
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
